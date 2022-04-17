3ba0c2aee7a8c0f20e2cd4f33f9ad2dc8d450030
Кристина Вячеславовна Стасюк <s0160150@edu.kubsu.ru>
2022-04-17 15:24:12 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp12/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace laba6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EM = new System.Windows.Forms.Button();
+            this.RK2 = new System.Windows.Forms.Button();
+            this.ClearAll = new System.Windows.Forms.Button();
+            this.iter = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.X0 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.Y0 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.h0 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // EM
+            // 
+            this.EM.BackColor = System.Drawing.Color.LightGray;
+            this.EM.Location = new System.Drawing.Point(964, 172);
+            this.EM.Name = "EM";
+            this.EM.Size = new System.Drawing.Size(184, 93);
+            this.EM.TabIndex = 0;
+            this.EM.Text = "Метод Эйлера";
+            this.EM.UseVisualStyleBackColor = false;
+            this.EM.Click += new System.EventHandler(this.BTN_EM);
+            // 
+            // RK2
+            // 
+            this.RK2.BackColor = System.Drawing.Color.LightGray;
+            this.RK2.Location = new System.Drawing.Point(964, 362);
+            this.RK2.Name = "RK2";
+            this.RK2.Size = new System.Drawing.Size(184, 95);
+            this.RK2.TabIndex = 3;
+            this.RK2.Text = "Метод Рунге-Кутта второго порядка";
+            this.RK2.UseVisualStyleBackColor = false;
+            this.RK2.Click += new System.EventHandler(this.BTN_RK2);
+            // 
+            // ClearAll
+            // 
+            this.ClearAll.BackColor = System.Drawing.Color.Gray;
+            this.ClearAll.Location = new System.Drawing.Point(847, 526);
+            this.ClearAll.Name = "ClearAll";
+            this.ClearAll.Size = new System.Drawing.Size(150, 59);
+            this.ClearAll.TabIndex = 8;
+            this.ClearAll.Text = "Очистить график";
+            this.ClearAll.UseVisualStyleBackColor = false;
+            this.ClearAll.Click += new System.EventHandler(this.BTN_Clear);
+            // 
+            // iter
+            // 
+            this.iter.BackColor = System.Drawing.Color.LightGray;
+            this.iter.Location = new System.Drawing.Point(730, 151);
+            this.iter.Name = "iter";
+            this.iter.Size = new System.Drawing.Size(144, 23);
+            this.iter.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(730, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 23);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Выберете функцию";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(730, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(184, 23);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Введите количество итераций";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(730, 242);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(173, 23);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Введите X0";
+            // 
+            // X0
+            // 
+            this.X0.BackColor = System.Drawing.Color.LightGray;
+            this.X0.Location = new System.Drawing.Point(733, 268);
+            this.X0.Name = "X0";
+            this.X0.Size = new System.Drawing.Size(141, 23);
+            this.X0.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(730, 312);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(173, 23);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "Введите Y0 ";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // Y0
+            // 
+            this.Y0.BackColor = System.Drawing.Color.LightGray;
+            this.Y0.Location = new System.Drawing.Point(733, 338);
+            this.Y0.Name = "Y0";
+            this.Y0.Size = new System.Drawing.Size(141, 23);
+            this.Y0.TabIndex = 23;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(733, 381);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(173, 23);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Введите шаг";
+            // 
+            // h0
+            // 
+            this.h0.BackColor = System.Drawing.Color.LightGray;
+            this.h0.Location = new System.Drawing.Point(733, 407);
+            this.h0.Name = "h0";
+            this.h0.Size = new System.Drawing.Size(141, 23);
+            this.h0.TabIndex = 25;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.LightGray;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "y\'=3x^(2)*y",
+            "y\'=3x^2y+x^2*e^(x^3)"});
+            this.comboBox1.Location = new System.Drawing.Point(730, 62);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(144, 23);
+            this.comboBox1.TabIndex = 30;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gray;
+            this.button3.Location = new System.Drawing.Point(847, 612);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 59);
+            this.button3.TabIndex = 31;
+            this.button3.Text = "Построить точный график";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(1160, 749);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.h0);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.Y0);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.X0);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.iter);
+            this.Controls.Add(this.ClearAll);
+            this.Controls.Add(this.RK2);
+            this.Controls.Add(this.EM);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button EM;
+        private System.Windows.Forms.Button RK2;
+        private System.Windows.Forms.Button ClearAll;
+        private System.Windows.Forms.TextBox iter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox X0;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox Y0;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox h;
+        private System.Windows.Forms.Label curStep;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox h0;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
+
---
+++ WinFormsApp12/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace laba6
+{
+    public partial class Form1 : Form
+    {
+        public static int n;
+        public string ME = "Метод Эйлера";
+        public string MRK2 = "Метод Рунге-Кутта второго порядка";
+        ZedGraphControl zedGrapgControl1 = new ZedGraphControl();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            zedGrapgControl1.Location = new Point(10, 30);
+            zedGrapgControl1.Name = "text";
+            zedGrapgControl1.Size = new Size(700, 700);
+            Controls.Add(zedGrapgControl1);
+            GraphPane my_Pane = zedGrapgControl1.GraphPane;
+            my_Pane.Title.Text = "График функции";
+            my_Pane.XAxis.Title.Text = " X";
+            my_Pane.YAxis.Title.Text = " Y";
+
+        }
+        private void GetSize()
+        {
+            zedGrapgControl1.Location = new Point(10, 10);
+            zedGrapgControl1.Size = new Size(ClientRectangle.Width - 20, ClientRectangle.Height - 20);
+
+        }
+ 
+        static double f1(double x, double y)
+        {
+            return 3*x*x*y ;
+        }
+        static double f2(double x, double y)
+        {
+            return 3*x*x*y+x*x*Math.Pow(Math.E,x*x*x);
+        }
+
+        static double f3(double x)
+        {
+            return (x * x * x * Math.Pow(Math.E, x * x * x)) / 3;
+        }
+
+        static double f4(double x)
+        {
+            return Math.Pow(Math.E, x * x * x);
+        }
+
+        private void Eiler(ZedGraphControl Zed_GraphControl)
+        {
+            GraphPane my_Pane = Zed_GraphControl.GraphPane;
+
+            PointPairList list = new PointPairList();
+
+            double h = Convert.ToDouble(h0.Text);
+            double y=  Convert.ToDouble(Y0.Text);
+            double x= Convert.ToDouble(X0.Text);
+            n = Convert.ToInt32(iter.Text);
+            switch (comboBox1.Text)
+                {
+                    case "y'=3x^(2)*y":
+                        {
+                            for (int i = 0; i < n + 1; i++)
+                            {
+                                list.Add(x, y);
+                                y = y + h * f1(x, y); 
+                                x += h;
+                            }
+                        }
+                        break;
+                    case "y'=3x^2y+x^2*e^(x^3)":
+                        {
+                            for (int i = 0; i < n + 1; i++)
+                            {
+                                list.Add(x, y);
+                                y = y + h * f2(x, y); 
+                                x += h;
+                            }
+                        }
+                        break;
+                }
+            LineItem myCircle = my_Pane.AddCurve(ME, list, Color.DeepPink, SymbolType.Circle);
+            zedGrapgControl1.AxisChange();
+            zedGrapgControl1.Invalidate();
+
+        }
+
+        private void RungeKutto2(ZedGraphControl Zed_GraphControl)
+        {
+            GraphPane my_Pane = Zed_GraphControl.GraphPane;
+           
+            PointPairList list = new PointPairList();
+
+            double h = Convert.ToDouble(h0.Text);
+            double y = Convert.ToDouble(Y0.Text);
+            double x = Convert.ToDouble(X0.Text);
+            n = Convert.ToInt32(iter.Text);
+
+
+            switch (comboBox1.Text)
+                {
+                    case "y'=3x^(2)*y":
+                        {
+                            for (int i = 0; i < n + 1; i++)
+                            {
+                                list.Add(x, y);
+                                double d = f1(x, y);
+                                y = y + (h / 2) * (d + f1(x + h, y + h * d)); //делаем шаг
+                                x += h;
+                            }
+                        }
+                        break;
+                    case "y'=3x^2y+x^2*e^(x^3)":
+                        {
+                            for (int i = 0; i < n + 1; i++)
+                            {
+                                list.Add(x, y);
+                                double d = f2(x, y);
+                                y = y + (h / 2) * (d + f2(x + h, y + h * d)); //делаем шаг
+                                x += h;
+                            }
+                        }
+                        break;
+                }
+                LineItem myCircle = my_Pane.AddCurve(MRK2, list, Color.DarkGoldenrod, SymbolType.Circle);
+                zedGrapgControl1.AxisChange();
+                zedGrapgControl1.Invalidate();
+        }
+
+        private void GriddenOn(GraphPane my_Pane)
+        {
+            my_Pane.XAxis.MajorGrid.IsVisible = true;
+            my_Pane.YAxis.MajorGrid.IsVisible = true;
+            my_Pane.YAxis.MinorGrid.IsVisible = true;
+            my_Pane.XAxis.MinorGrid.IsVisible = true;
+        }
+        
+        private void Clear(ZedGraphControl Zed_GraphControl)
+        {
+            //GraphPane pane = Zed_GraphControl.GraphPane;
+            zedGrapgControl1.GraphPane.CurveList.Clear();
+            zedGrapgControl1.GraphPane.GraphObjList.Clear();
+
+            zedGrapgControl1.GraphPane.XAxis.Type = AxisType.Linear;
+            zedGrapgControl1.GraphPane.XAxis.Scale.TextLabels = null;
+            zedGrapgControl1.GraphPane.XAxis.MajorGrid.IsVisible = false;
+            zedGrapgControl1.GraphPane.YAxis.MajorGrid.IsVisible = false;
+            zedGrapgControl1.GraphPane.YAxis.MinorGrid.IsVisible = false;
+            zedGrapgControl1.GraphPane.XAxis.MinorGrid.IsVisible = false;
+            zedGrapgControl1.RestoreScale(zedGrapgControl1.GraphPane);
+
+            zedGrapgControl1.AxisChange();
+            zedGrapgControl1.Invalidate();
+        }
+        private void BTN_EM(object sender, EventArgs e)
+        {
+            GriddenOn(zedGrapgControl1.GraphPane);
+            Eiler(zedGrapgControl1);
+
+        }
+
+
+        private void BTN_RK2(object sender, EventArgs e)
+        {
+            GriddenOn(zedGrapgControl1.GraphPane);
+
+            RungeKutto2(zedGrapgControl1);
+        }
+
+        private void BTN_Clear(object sender, EventArgs e)
+        {
+            Clear(zedGrapgControl1);
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void toch(ZedGraphControl Zed_GraphControl)
+        {
+            GraphPane my_Pane = Zed_GraphControl.GraphPane;
+            PointPairList list = new PointPairList();
+            double h = Convert.ToDouble(h0.Text);
+            double y = Convert.ToDouble(Y0.Text);
+            double x = Convert.ToDouble(X0.Text);
+            n = Convert.ToInt32(iter.Text);
+            double i;
+
+            switch (comboBox1.Text)
+            {
+                case "y'=3x^(2)*y":
+                    {
+                        for (i = 0; i < 1; i+=h)
+                        {
+                            list.Add(i, f4(i));
+
+                        }
+                    }
+                    break;
+                case "y'=3x^2y+x^2*e^(x^3)":
+                    {
+
+                        for (i = 0; i <  1; i+=h)
+                        {
+                            list.Add(i, f3(i));
+                        }
+
+
+                    }
+                    break;
+            }
+            LineItem myCircle = my_Pane.AddCurve(MRK2, list, Color.Black, SymbolType.Circle);
+            zedGrapgControl1.AxisChange();
+            zedGrapgControl1.Invalidate();
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            GriddenOn(zedGrapgControl1.GraphPane);
+
+            toch(zedGrapgControl1);
+        }
+
+    }
+}
